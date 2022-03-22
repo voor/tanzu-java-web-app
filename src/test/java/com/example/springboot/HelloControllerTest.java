44c9a91e0805ee5c172de0cfc4ead001a2fef2c1
tputbrese <tputbrese@vmware.com>
2022-03-22 16:06:59 -0400

    Minor text change

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot, Tanzu, and src, test, java, com, example, springboot!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
     }
 }
