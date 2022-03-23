83786d9f3efa25bdb6fc9cd0be0034caab2ca00f
tputbrese <tputbrese@vmware.com>
2022-03-22 22:58:35 -0400

    minor change

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot, Trevor, and Tanzu!";
+		return "Greetings from Spring Boot and Tanzu!";
 	}
 
 }
